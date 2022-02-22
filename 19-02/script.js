ae470be1de68f3440b67465490c8306409d6a45b
z to zee h <zhoomart.d@gmail.com>
2022-02-22 20:11:50 +0600

    feat: 19-02 task

---
+++ 19-02/script.js
@@ -0,0 +1,14 @@
+let name1 = 'Joomart';
+let name2 = 'Duishenaliev';
+name1 = name2;
+alert(name2);
+
+
+let name = 'Joomart';
+let string1 = `Привет, ${name}!`;
+alert(string1);
+
+
+let age = 29;
+let string2 = `Мне ${age} лет!`;
+alert(string2);
