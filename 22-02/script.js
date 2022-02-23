99accb13f188e01a9576ed9752fa1a0eb0b188ee
z to zee h <zhoomart.d@gmail.com>
2022-02-23 15:02:55 +0600

    feat: all tasks done

--- 22-02/script.js
+++ 22-02/script.js
@@ -4,6 +4,17 @@ let ThirdNum = 2;
 [firstNum, second_num, ThirdNum].forEach(alert);
 
 
+
 let str1 = 'САЛАМ, ';
 let str2 = `${str1} БИШКЕК!`;
 alert(str2);
+
+
+
+let a = 17;
+let b = 10;
+let c = a - b;
+
+let d = 7;
+let result = c + d;
+console.log(result);
