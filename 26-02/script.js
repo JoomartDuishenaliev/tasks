b5c2d309f2b455f77a3a4e9986ac99b9799bfc22
z to zee h <zhoomart.d@gmail.com>
2022-02-27 18:15:43 +0600

    feat: task 1,2,3 done

---
+++ 26-02/script.js
@@ -0,0 +1,35 @@
+//1
+let x = 8;
+if(x === 7){
+    alert('Верно');
+} else{
+    alert('Неверно');
+}
+
+
+//2
+let a = 1;
+let b = 3;
+if(a <= 1 && b >= 3){
+    alert(a + b);
+} else{
+    alert(b - a);
+}
+
+
+//3
+let time = 60;
+if(time >= 0 && time <= 14){
+    alert('Первая четверть');
+} else if(time >= 15 && time <= 29){
+    alert('Вторая четверть');
+} else if(time >= 30 && time <= 44){
+    alert('Третья четверть');
+} else if(time >= 45 && time <= 59){
+    alert('Четвертая четверть');
+} else{
+    alert('Значение не попадает в диапазон от 0 до 59');
+}
+
+
+//4
