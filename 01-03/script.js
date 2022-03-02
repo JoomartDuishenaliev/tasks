1af9be874779f2099eda4194ad157262ec371a37
z to zee h <zhoomart.d@gmail.com>
2022-03-02 15:56:22 +0600

    feat: all tasks completed

---
+++ 01-03/script.js
@@ -0,0 +1,23 @@
+//1
+let userSalaries = {
+    ella:40000,
+    sophia:50000,
+    ellie:45000
+};
+console.log(userSalaries.ella, userSalaries.sophia);
+
+
+//2
+let nums = {
+    a:5,
+    b:6,
+    c:10
+};
+let result = nums.a + nums.b + nums.c;
+console.log(result);
+
+
+//3
+let data = false;
+let number = data === true ? 3 : 5;
+console.log(number);
