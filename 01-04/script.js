9aa9011cdc3e86bcb2ad18c583850ee34dd3a0f0
z to zee h <zhoomart.d@gmail.com>
2022-03-04 13:09:28 +0600

    feat: task 01-04 completed

--- 01-04/script.js
+++ 01-04/script.js
@@ -1,3 +1,25 @@
+//1
 let arr = ['a', 'b', 'c', 'd'];
-let string = arr[0] + '+' + arr[1] + ', ' + arr[2] + '+' + arr[3];
-console.log(string);
+let concat = arr[0] + '+' + arr[1] + ', ' + arr[2] + '+' + arr[3];
+console.log(concat);
+
+
+//2
+let data = [10, 15, 9, 10];
+let a = data[0] * data[1];
+let b = data[2] * data[3];
+let result = a + b;
+console.log(result);
+
+
+//3
+let array1 = ['a', 'b', 'c'];
+array1.push(1, 2, 3);
+console.log(array1);
+
+
+let array2 = ['a', 'b', 'c'];
+array2[3] = 1;
+array2[4] = 2;
+array2[5] = 3;
+console.log(array2);
