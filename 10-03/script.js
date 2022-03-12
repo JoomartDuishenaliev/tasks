93997252747a3d82d4d6872b8df821b24c87bc0b
z to zee h <zhoomart.d@gmail.com>
2022-03-12 10:47:38 +0600

    feat: arrays into object task added

---
+++ 10-03/script.js
@@ -0,0 +1,18 @@
+let justArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+let evenArray = [];
+let oddArray = [];
+
+for(let i = 0; i < justArray.length; i++){
+    if(justArray[i] % 2 === 0) {
+        evenArray.push(justArray[i]);
+    } else {
+        oddArray.push(justArray[i]);
+    }
+}
+
+let justObject = {
+    odd:oddArray,
+    even:evenArray
+};
+
+console.log(justObject);
