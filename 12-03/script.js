d30e543fddeb85808f8cf26d1181e60bd2abdc93
z to zee h <zhoomart.d@gmail.com>
2022-03-14 21:18:16 +0600

    feat: all tasks done

--- 12-03/script.js
+++ 12-03/script.js
@@ -1,6 +1,7 @@
 
 //1
 let hello = () => 'Привет, Javascript';
+console.log(hello());
 
 
 //2
@@ -9,3 +10,19 @@ console.log(numCube(3));
 
 
 //3
+let name = 'Василий';
+
+let getName = () => (name == null || name !== 'Василий' ) ? `Привет, гость` : `Привет, ${name}`;
+console.log(getName());
+
+
+//4
+let array1 = [1, 2, 3];
+let array2 = [4, 1, 1, 1, 4];
+let array3 = [2, 2, 2, 2, 2, 2];
+
+let double1 = array1.map(currentValue => currentValue + currentValue);
+let double2 = array2.map(currentValue => currentValue + currentValue);
+let double3 = array3.map(currentValue => currentValue + currentValue);
+
+console.log(double1, double2, double3);
