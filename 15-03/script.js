7e238a4e61c09dc7f0e7bf968abbcf07722982ca
z to zee h <zhoomart.d@gmail.com>
2022-03-15 23:50:47 +0600

    init: task 15-03

---
+++ 15-03/script.js
@@ -0,0 +1,22 @@
+const goods = [
+    {
+        name:'Iphone 12',
+        price:790
+    },
+    {
+        name:'Iphone 12 pro',
+        price:800
+    },
+    {
+        name:'Iphone 12 pro max',
+        price:1050
+    },
+    {
+        name:'Iphone 12 pro',
+        price:1000
+    },
+    {
+        name:'Iphone 13 pro max',
+        price:1200
+    }
+]
