251734b3d906f9909a8d125b159aeda8f7501150
z to zee h <zhoomart.d@gmail.com>
2022-03-18 21:48:06 +0600

    feat: tasks done

---
+++ 17-03/script.js
@@ -0,0 +1,22 @@
+//1
+function updateContent(element, length){
+    let newArr = [];
+    for(let i = 0; i < length; i++){
+        newArr.push(element);
+    }
+    console.log(newArr);
+}
+updateContent('a', 3);
+
+
+//2
+let arr = [-5,10,5,55,-1,22,-4,36,-45];
+
+let result = arr.reduce(function(sum, elem){
+    if(elem >= 0){
+        return sum + elem;
+    } else{
+        return sum;
+    }
+}, 0);
+console.log(result);
