84ad7f38ce7d6125490ae7fd34e0d60a172932f6
z to zee h <zhoomart.d@gmail.com>
2022-03-25 02:18:53 +0600

    feat: timer function added

--- 24-03/script.js
+++ 24-03/script.js
@@ -7,7 +7,6 @@ const countNums = (x) => {
     return `Количество чисел = ${x}`;
 }
 
-
 const addNums = (q, sum) => {
     let count = Number(prompt('Введите число'));
     while(count != 999){
@@ -18,9 +17,32 @@ const addNums = (q, sum) => {
     return `Количество чисел = ${q} ; Сумма чисел = ${sum}`;
 }
 
-
 const showResult = (text) => {
     console.log(text);
 }
 showResult(countNums(0));
 showResult(addNums(0, 0));
+
+
+//1
+const showAge = () => {
+    let inputAge = Number(prompt('Введите ваш год рождения'));
+    let inputYear = Number(prompt('Введите нынешний год'));
+    return `Вам ${inputYear - inputAge} лет`;
+}
+console.log(showAge());
+
+
+//2
+const sayHi = (text) => {
+    let count = 0;
+    const intervalId = setInterval(() => {
+        console.log(text);
+        count++;
+        if(count === 5){
+            clearInterval(intervalId);
+            console.log('Done');
+        }
+    }, 1000)
+}
+sayHi('Hello world');
