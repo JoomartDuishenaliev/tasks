58b813b8ee98679e49823284846c7a6c853e8b91
z to zee h <zhoomart.d@gmail.com>
2022-03-28 01:31:12 +0600

    fix: timer endless count fixed

--- 26-03/script.js
+++ 26-03/script.js
@@ -2,17 +2,19 @@ const btn = document.querySelector('.btn');
 const notification = document.querySelector('.notification');
 
 function showNotification() {
-    notification.className = 'alert alert-secondary col mt-3 fw-bold text-center text-primary';
-    notification.textContent = 'Oopsie ошибочка. Обновите страницу.';
-    btn.className = 'btn btn-outline-warning col-10 mt-3 me-5';
+    notification.className = 'alert alert-warning col mt-3 fw-bold text-center text-primary fs-5';
+    notification.style.height = '100px';
+    notification.textContent = 'Oopsie, ошибочка. Обновите страницу.';
+    btn.className = 'btn btn-outline-warning col-10 mt-4 me-5';
 
     let count = 0;
-    setInterval(() => {
+    let intervalId = setInterval(() => {
         count++;
         if(count === 1){
             notification.className = '';
             notification.textContent = '';
-            btn.className = 'btn btn-primary col-10 mt-3 me-5';
+            btn.className = 'btn btn-primary col-10 mt-4 me-5';
+            clearInterval(intervalId);
         }
     }, 1500)
 }
