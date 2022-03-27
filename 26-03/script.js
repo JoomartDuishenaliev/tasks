e62fe1afe6d8c3a0788f6cc38154262e8bd45441
z to zee h <zhoomart.d@gmail.com>
2022-03-27 19:29:29 +0600

    feat: alert with timer completed

--- 26-03/script.js
+++ 26-03/script.js
@@ -1,13 +1,20 @@
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('.btn');
 const notification = document.querySelector('.notification');
 
 function showNotification() {
-    notification.className = 'new-notification';
-    notification.textContent = 'Oops. Обновите браузер.';
-    notification.style.fontSize = '22px';
-    notification.style.textAlign = 'center';
-    notification.style.color = 'purple';
-    notification.style.paddingTop = '30px';
+    notification.className = 'alert alert-secondary col mt-3 fw-bold text-center text-primary';
+    notification.textContent = 'Oopsie ошибочка. Обновите страницу.';
+    btn.className = 'btn btn-outline-warning col-10 mt-3 me-5';
+
+    let count = 0;
+    setInterval(() => {
+        count++;
+        if(count === 1){
+            notification.className = '';
+            notification.textContent = '';
+            btn.className = 'btn btn-primary col-10 mt-3 me-5';
+        }
+    }, 1500)
 }
 
 btn.addEventListener('click', showNotification);
